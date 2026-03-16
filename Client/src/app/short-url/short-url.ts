33a7e2c996f51cb8058ed4ef3d46f66fd5ae1376
Sohith <sohithreddy@apptmyz.com>
2026-03-17 00:26:46 +0530

    Added ShortUrl component with template and styles

---
+++ Client/src/app/short-url/short-url.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-short-url',
+  imports: [],
+  templateUrl: './short-url.html',
+  styleUrl: './short-url.css',
+})
+export class ShortUrl {}
