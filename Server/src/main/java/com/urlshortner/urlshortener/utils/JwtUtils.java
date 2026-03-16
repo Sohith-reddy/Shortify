b4e57a0a60ab062807f4780e91f580aa8ef0c131
Sohith <sohithreddy@apptmyz.com>
2026-03-16 22:35:21 +0530

    Integrated JWT in the application

--- Server/src/main/java/com/urlshortner/urlshortener/utils/JwtUtils.java
+++ Server/src/main/java/com/urlshortner/urlshortener/utils/JwtUtils.java
@@ -1,5 +1,65 @@
 package com.urlshortner.urlshortener.utils;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
 public class JwtUtils {
 
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.token.expiry-in-min}")
+    private long jwtExpiryMinutes;
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiryMinutes * 60 * 1000))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(jwtSecret)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+    public boolean validateToken(String token) {
+
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(jwtSecret)
+                    .build()
+                    .parseClaimsJws(token);
+
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    public Date getExpirationDate(String token) {
+
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(jwtSecret)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getExpiration();
+    }
 }
