2249eace626b81ea20227a789bdb695351c4b681
Sohith <sohithreddy@apptmyz.com>
2026-03-17 13:43:03 +0530

    Updating the UI using Angular

--- Client/src/app/short-url/short-url.ts
+++ Client/src/app/short-url/short-url.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-short-url',
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './short-url.html',
   styleUrl: './short-url.css',
 })
-export class ShortUrl {}
+export class ShortUrl {
+  shortUrlForm!:FormGroup;
+  constructor(private fb:FormBuilder){
+    this.shortUrlForm = this.fb.group({
+      originalUrl:['',Validators.compose([Validators.required,Validators.pattern('https?://.+')])],
+      customAlias:['',Validators.pattern('^[a-zA-Z0-9_-]+$')],
+      expirationTime:[new Date(),Validators.pattern('^\\d+(s|m|h|d)$')]
+    });
+  }
+  ngOnInit(): void {
+
+  }
+  
+}
